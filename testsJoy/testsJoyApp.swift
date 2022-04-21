8a013ec7de3160d467cfcaa7521150b0341a2836
SaraRaquelRibeiro <lins.ribeiro.001@gmail.com>
2022-04-21 14:23:47 +0100

    Initial Commit

---
+++ testsJoy/testsJoyApp.swift
@@ -0,0 +1,17 @@
+//
+//  testsJoyApp.swift
+//  testsJoy
+//
+//  Created by Sara Ribeiro on 21/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct testsJoyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
